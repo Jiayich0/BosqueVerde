dd8c5ccdad5e849d1d056dcbce7d5b7962ecf1b0
jialewu16 <147049773+jialewu16@users.noreply.github.com>
2026-04-07 02:29:54 +0200

    Update SistemaBDL.java

--- src/bosquedeletras/facade/SistemaBDL.java
+++ src/bosquedeletras/facade/SistemaBDL.java
@@ -13,6 +13,7 @@ public class SistemaBDL {
 	private SistemaBDL() {
 		this.controlVendedor = new ControlVendedor();
 		this.controlLibro = new ControlLibro();
+		this.controlFactura = new ControlFactura();
 	}
 
 	public static SistemaBDL getInstance() {
@@ -62,5 +63,41 @@ public class SistemaBDL {
 
 	public java.util.List<bosquedeletras.model.Libro> listarLibros() {
 		return controlLibro.listarLibros();
+	}
+
+	public boolean abrirVenta(String fecha, int idCliente, int idVendedor) {
+		return controlFactura.abrirVenta(fecha, idCliente, idVendedor);
+	}
+
+	public boolean anadirLibroAVenta(int idFactura, int idLibro, int cantidad, double precioUnitario) {
+		return controlFactura.anadirLibroAVenta(idFactura, idLibro, cantidad, precioUnitario);
+	}
+
+	public boolean cerrarVenta(int idFactura) {
+		return controlFactura.cerrarVenta(idFactura);
+	}
+
+	public double calcularTotal(int idFactura) {
+		return controlFactura.calcularTotal(idFactura);
+	}
+
+	public Factura leerFactura(int idFactura) {
+		return controlFactura.leerFactura(idFactura);
+	}
+
+	public List<LineaFactura> leerLineasFactura(int idFactura) {
+		return controlFactura.leerLineasFactura(idFactura);
+	}
+
+	public List<Factura> listarFacturas() {
+		return controlFactura.listarFacturas();
+	}
+
+	public List<Factura> listarFacturasPorCliente(int idCliente) {
+		return controlFactura.listarFacturasPorCliente(idCliente);
+	}
+
+	public List<Factura> listarFacturasPorVendedor(int idVendedor) {
+		return controlFactura.listarFacturasPorVendedor(idVendedor);
 	}
 }
