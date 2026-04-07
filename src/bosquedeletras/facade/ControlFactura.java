3719930ed04b7ea6a41fe48ca9537b93b05c8bd5
jialewu16 <147049773+jialewu16@users.noreply.github.com>
2026-04-07 02:27:19 +0200

    Update ControlFactura.java

--- src/bosquedeletras/facade/ControlFactura.java
+++ src/bosquedeletras/facade/ControlFactura.java
@@ -1,1 +1,94 @@
+package bosquedeletras.facade;
 
+import java.util.List;
+
+import bosquedeletras.dao.DAOFactura;
+import bosquedeletras.dao.DAOLineaFactura;
+import bosquedeletras.model.Factura;
+import bosquedeletras.model.LineaFactura;
+
+public class ControlFactura {
+
+	private DAOFactura daoFactura;
+	private DAOLineaFactura daoLineaFactura;
+
+	public ControlFactura() {
+		this.daoFactura = new DAOFactura();
+		this.daoLineaFactura = new DAOLineaFactura();
+	}
+
+	public boolean abrirVenta(String fecha, int idCliente, int idVendedor) {
+		Factura nuevaFactura = new Factura(fecha, idCliente, idVendedor);
+		return daoFactura.insertar(nuevaFactura);
+	}
+
+	public boolean anadirLibroAVenta(int idFactura, int idLibro, int cantidad, double precioUnitario) {
+		Factura factura = daoFactura.buscarPorId(idFactura);
+
+		if (factura == null || factura.isCerrada()) {
+			return false;
+		}
+
+		LineaFactura nuevaLinea = new LineaFactura(idFactura, idLibro, cantidad, precioUnitario);
+		boolean insertada = daoLineaFactura.insertar(nuevaLinea);
+
+		if (!insertada) {
+			return false;
+		}
+
+		double nuevoTotal = calcularTotal(idFactura);
+		factura.setTotal(nuevoTotal);
+
+		return daoFactura.actualizar(factura);
+	}
+
+	public boolean cerrarVenta(int idFactura) {
+		Factura factura = daoFactura.buscarPorId(idFactura);
+
+		if (factura == null || factura.isCerrada()) {
+			return false;
+		}
+
+		List<LineaFactura> lineas = daoLineaFactura.listarPorFactura(idFactura);
+		if (lineas == null || lineas.isEmpty()) {
+			return false;
+		}
+
+		double total = calcularTotal(idFactura);
+		factura.setTotal(total);
+		factura.setCerrada(true);
+
+		return daoFactura.actualizar(factura);
+	}
+
+	public double calcularTotal(int idFactura) {
+		List<LineaFactura> lineas = daoLineaFactura.listarPorFactura(idFactura);
+
+		double total = 0.0;
+		for (LineaFactura linea : lineas) {
+			total += linea.getSubtotal();
+		}
+
+		return total;
+	}
+
+	public Factura leerFactura(int idFactura) {
+		return daoFactura.buscarPorId(idFactura);
+	}
+
+	public List<LineaFactura> leerLineasFactura(int idFactura) {
+		return daoLineaFactura.listarPorFactura(idFactura);
+	}
+
+	public List<Factura> listarFacturas() {
+		return daoFactura.listar();
+	}
+
+	public List<Factura> listarFacturasPorCliente(int idCliente) {
+		return daoFactura.listarPorCliente(idCliente);
+	}
+
+	public List<Factura> listarFacturasPorVendedor(int idVendedor) {
+		return daoFactura.listarPorVendedor(idVendedor);
+	}
+}
